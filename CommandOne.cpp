6e94edcbf16e4996300f08656030103f5025e133
Ahiya Schneider <achiaschnider@hotmail.com>
2023-01-16 22:07:19 +0200

    the validator moved to client so delete her and not write in end

--- CommandOne.cpp
+++ CommandOne.cpp
@@ -5,22 +5,23 @@ using namespace std;
 void CommandOne::execute() {
     Command::dio->write("Please upload your local train CSV file.\n");
     string path_to_class = Command::dio->read();
-    if(!validator.validFile(path_to_class)) {
-        Command::dio->write("invalid input\n");
+    if (path_to_class[0] == '1')
+    {
         return;
     }
-    my_data.set_class(path_to_class);
+
     
-    Command::dio->write("Please upload your local test CSV file.");
+    Command::dio->write("Please upload your local test CSV file.\n");
     string path_to_unclass = Command::dio->read();
-    if(!validator.validFile(path_to_unclass)) {
-        Command::dio->write("invalid input");
+    if (path_to_unclass[0] == '1')
+    {
         return;
     }
     
+    my_data.set_class(path_to_class);   
     my_data.set_unclass(path_to_unclass);
     //let all other commands know
     update_after_executed(my_data);
     //here we will forward the path to the server and make sure it can be opened - if not - need to change message!
-    Command::dio->write("upload complete.");
+    //Command::dio->write("upload complete.");
 }
