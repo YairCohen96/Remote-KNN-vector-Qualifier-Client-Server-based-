19594769b039483ae710747d775ab1abf6788c9c
Ahiya Schneider <achiaschnider@hotmail.com>
2023-01-17 10:46:53 +0200

    after every write added: string retToMen2 = Command::dio->read(); read string so it can keep send

--- CommandFour.cpp
+++ CommandFour.cpp
@@ -9,17 +9,21 @@ void CommandFour::execute() {
     //check avalability for runchecking if not exist print "data upload please."
     if(!validator.existFiles(my_data.unclass_path, my_data.class_path)){
         Command::dio->write("please upload data.\n");
+        string retToMen = Command::dio->read();
     } else if (!my_data.results.size())
     {
         Command::dio->write("please classify the data.\n");
+        string retToMen1 = Command::dio->read();
     } else {
         int i, endI = my_data.results.size();
         for (i = 0; i < endI; i++)
         {
             //string s = to_string(my_data.results.at(i).first) + "\t" + my_data.results.at(i).second + "\n";
             Command::dio->write(to_string(my_data.results.at(i).first) + "\t" + my_data.results.at(i).second + "\n");
+            string retToMen2 = Command::dio->read();
         }
         Command::dio->write("done.\n");
+        string retToMen3 = Command::dio->read();
     }
     
 }
