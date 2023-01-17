afc81e9a44db8d919ed2c0cddfa51e62bf3a31a4
Yair Cohen <yaish81@gmail.com>
2023-01-17 16:15:15 +0200

    1 and 2 works. need to check listener for all

--- CommandOne.cpp
+++ CommandOne.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 /**
  * execute - emplmntation of command header.
-*/
+ */
 
-void CommandOne::execute() {
+void CommandOne::execute()
+{
     Command::dio->write("Please upload your local train CSV file.\n");
     string path_to_class = Command::dio->read();
     if (path_to_class[0] == '1')
@@ -14,27 +15,27 @@ void CommandOne::execute() {
         return;
     }
 
-    
     Command::dio->write("Please upload your local test CSV file.\n");
     string path_to_unclass = Command::dio->read();
 
-    if(!validator.validFile(path_to_unclass)) {
+    /*if (!validator.validFile(path_to_unclass))
+    {
         Command::dio->write("invalid input\n");
+    }*/
 
     if (path_to_unclass[0] == '1')
     {
 
         return;
     }
-    
-    my_data.set_class(path_to_class);   
+
+    my_data.set_class(path_to_class);
     my_data.set_unclass(path_to_unclass);
-    //let all other commands know
+    // let all other commands know
     update_after_executed(my_data);
-    //here we will forward the path to the server and make sure it can be opened - if not - need to change message!
+    // here we will forward the path to the server and make sure it can be opened - if not - need to change message!
 
-    Command::dio->write("upload complete.\n");
+    //Command::dio->write("upload complete.\n");
 
-    //Command::dio->write("upload complete.");
-
+    // Command::dio->write("upload complete.");
 }
